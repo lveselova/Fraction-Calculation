773004820ab11c487e3fe4bcd73c285703b4d99e
lveselova <lizaveselova15@gmail.com>
2022-04-21 12:39:16 -0700

    Add build-dir

---
+++ build-dir/CMakeFiles/Fraction_Calculation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fraction_Calculation.dir/driver.cpp.o"
+  "CMakeFiles/Fraction_Calculation.dir/driver.cpp.o.d"
+  "CMakeFiles/Fraction_Calculation.dir/fraction.cpp.o"
+  "CMakeFiles/Fraction_Calculation.dir/fraction.cpp.o.d"
+  "Fraction_Calculation"
+  "Fraction_Calculation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fraction_Calculation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-dir/CMakeFiles/Fraction_Calculation.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Fraction_Calculation.
+# This may be replaced when dependencies are built.
